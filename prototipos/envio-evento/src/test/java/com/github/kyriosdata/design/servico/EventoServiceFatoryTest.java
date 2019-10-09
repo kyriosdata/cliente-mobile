0e54019bd2b820651c7726103acc5b6755828dd1
kyriosdata <fabio@inf.ufg.br>
2019-10-09 16:37:41 -0300

    revisao de testes

--- prototipos/envio-evento/src/test/java/com/github/kyriosdata/design/servico/EventoServiceFatoryTest.java
+++ prototipos/envio-evento/src/test/java/com/github/kyriosdata/design/servico/EventoServiceFatoryTest.java
@@ -7,10 +7,20 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class EventoServiceFatoryTest {
 
     @Test
-    void experimento() {
+    void quandoNaoHaServicoDeRede() {
         EventoService servico = new EventoServiceFactory().getInstance();
         String enviado = servico.envia(() -> "mensagem");
         assertTrue(enviado.contains("mensagem"));
         assertTrue(enviado.contains("STORE AND FORWARD"));
+    }
+
+    @Test
+    void quandoHaServicoDeRede() {
+        EventoService servico = new EventoServiceFactory().getInstance();
+        servico.setFuncaoQueVerificaDisponibilidadeDeRede(() -> true);
+
+        String enviado = servico.envia(() -> "mensagem");
+        assertTrue(enviado.contains("mensagem"));
+        assertTrue(enviado.contains("SEND"));
     }
 }
